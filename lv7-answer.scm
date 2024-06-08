91a8337dd7e4c07ffe59a038ae55418d0c542551
Zhao Yuanyuan <zhaoyuanyuan@stu.pku.edu.cn>
2024-06-08 22:43:36 +0800

    two extra functions

--- lv7-answer.scm
+++ lv7-answer.scm
@@ -1,7 +1,14 @@
-(define (less x lst) (filter (lambda (a) (> x a)) lst))
-(define (greater x lst) (filter (lambda (a) (<= x a)) lst))
+(define (less x lst) 
+        (filter (lambda (a) (> x a)) lst))
+(define (greater x lst) 
+        (filter (lambda (a) (<= x a)) lst))
 
-(define (quicksort lst) (if (null? lst) '() (append (quicksort (less (car lst) (cdr lst))) (list (car lst)) (quicksort (greater (car lst) (cdr lst))))))
+(define (quicksort lst) 
+        (if (null? lst) 
+            '() 
+            (append (quicksort (less (car lst) (cdr lst))) 
+                    (list (car lst)) 
+                    (quicksort (greater (car lst) (cdr lst))))))
 (define lst '(12 71 2 15 29 82 87 8 18 66 81 25 63 97 40 3 93 58 53 31 47))
 
 (print (quicksort lst))
--- src/main.cpp
+++ src/main.cpp
@@ -17,19 +17,43 @@ struct TestCtx {
         return result->toString();
     }
 };
-bool checkBracket(const std::string& text) {
+int checkBracket(const std::string& text) {
     const char* cstr = text.c_str();
     std::deque<char> stack;
     for (int i = 0; i < strlen(cstr); ++i) {
         if (cstr[i] == '(') {
             stack.push_back('(');
         } else if(cstr[i] == ')') {
-            if (stack.empty()) return false;
+            if (stack.empty()) return 2; //2代表右括号多了
             stack.pop_back();
         }
     }
-    if (stack.empty()) return true;
-    return false;
+    if (stack.empty()) return 0; //0代表括号匹配
+    return 1; //1代表左括号多了
+}
+
+void neglectComment(std::string& line) {
+    size_t pos = line.find(';'); //忽略注释中的括号，把;后的内容删掉
+    if (pos != std::string::npos) {
+        line = line.substr(0, pos);
+    }
+}
+
+std::vector<std::string> splitExpressions(const std::string& line) {
+    std::vector<std::string> expressions;
+    std::string currentExpression;
+    for (char c : line) { 
+        currentExpression += c;
+        if (currentExpression == " ") {
+            currentExpression = "";
+            continue;
+        }
+        if (checkBracket(currentExpression) == 0) { //完整表达式
+            expressions.push_back(currentExpression);
+            currentExpression = "";
+        }
+    }
+    return expressions;
 }
 
 int main(int argc, char* argv[]) {
@@ -54,17 +78,32 @@ int main(int argc, char* argv[]) {
         // Standard input mode
         while (true) {
             try {
-                std::string text;
                 std::cout << ">>> " ;
-                do {
+                std::getline(std::cin, line);
+                if (std::cin.eof()) {
+                    std::cin.clear(); 
+                    continue;
+                }
+                neglectComment(line);
+                std::string text = line;
+                while (checkBracket(text)) {
+                    if (checkBracket(text) == 2) {
+                        throw SyntaxError("too much \')\'");
+                    }
                     std::getline(std::cin, line);
+                    if (std::cin.eof()) continue;
+                    neglectComment(line);
                     text += line;
-                } while (!checkBracket(text)); //读入直到括号匹配再tokenize
-                auto tokens = Tokenizer::tokenize(text);
-                Parser parser(std::move(tokens)); //含有一个token的deque
-                auto value = parser.parse(); //一个ValuePtr的deque
-                auto result = env->eval(std::move(value));
-                std::cout << result->toString() << std::endl; // 输出外部表示
+                } //读入直到括号匹配再tokenize
+                std::vector<std::string> expressions = splitExpressions(text); //对每个表达式求值
+                if (text == "") continue;
+                for (const std::string& expression : expressions) {
+                    auto tokens = Tokenizer::tokenize(expression);
+                    Parser parser(std::move(tokens)); //含有一个token的deque
+                    auto value = parser.parse(); //一个ValuePtr的deque
+                    auto result = env->eval(std::move(value));
+                    std::cout << result->toString() << std::endl; // 输出外部表示
+                }
             } catch (std::runtime_error& e) {
                 std::cerr << "Error: " << e.what() << std::endl;
             }
@@ -72,17 +111,27 @@ int main(int argc, char* argv[]) {
     } else if (mode == 2) {
         // File input mode
         while (std::getline(file, line)) {
-            if (line == "") continue;
+            if (line == "") continue; //空行
+            size_t pos = line.find(';'); //忽略注释中的括号
+            if (pos != std::string::npos) {
+                line = line.substr(0, pos);
+            }
             try {
                 std::string text = line;
-                while (!checkBracket(text)) { //读入直到括号匹配
+                while (checkBracket(text)) { //读入直到括号匹配，多行读入
+                    if (checkBracket(text) == 2) {
+                        throw SyntaxError("too much \')\'");
+                    }
                     if (std::getline(file, line)) {
+                        size_t pos = line.find(';');
+                        if (pos != std::string::npos) {
+                        line = line.substr(0, pos);
+                        }
                         text +=line;
                     } else {
                         throw SyntaxError("dismatched brackets");
                     }
                 }
-                
                 auto tokens = Tokenizer::tokenize(text);
                 Parser parser(std::move(tokens));
                 auto value = parser.parse();
